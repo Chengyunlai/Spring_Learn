e5ed9cf0a7376c684778deb792f7f961a7cc5bcf
chengyunlai <1257997441@qq.com>
2022-11-09 14:27:45 +0800

    注解驱动 和 组件扫描

---
+++ src/main/java/spring_02_06_12_basic/basic_07/annotation/bean/Cat.java
@@ -0,0 +1,29 @@
+package spring_02_06_12_basic.basic_07.annotation.bean;
+
+/**
+ * @ClassName
+ * @Description
+ * @Author:chengyunlai
+ * @Date
+ * @Version 1.0
+ **/
+public class Cat {
+    private String catName;
+    private Person master;
+
+    public String getCatName() {
+        return catName;
+    }
+
+    public void setCatName(String catName) {
+        this.catName = catName;
+    }
+
+    public Person getMaster() {
+        return master;
+    }
+
+    public void setMaster(Person master) {
+        this.master = master;
+    }
+}
---
+++ src/main/java/spring_02_06_12_basic/basic_07/annotation/bean/Dog.java
@@ -0,0 +1,16 @@
+package spring_02_06_12_basic.basic_07.annotation.bean;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @ClassName
+ * @Description
+ * @Author:chengyunlai
+ * @Date
+ * @Version 1.0
+ **/
+// 代表该类会被注册到 IOC 容器中作为一个 Bean
+@Component("MyDog") // 容器中id为MyDog
+public class Dog {
+    private String name;
+}
