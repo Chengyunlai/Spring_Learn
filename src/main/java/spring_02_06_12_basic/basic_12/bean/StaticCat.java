c2e3713beade39a5e32ad87d53619b0939dadecd
chengyunlai <1257997441@qq.com>
2022-11-11 21:12:25 +0800

    Bean的实例化方法

---
+++ src/main/java/spring_02_06_12_basic/basic_12/bean/StaticCat.java
@@ -0,0 +1,15 @@
+package spring_02_06_12_basic.basic_12.bean;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @ClassName
+ * @Description 使用静态工厂
+ * @Author:chengyunlai
+ * @Date
+ * @Version 1.0
+ **/
+
+public class StaticCat {
+
+}
