bf6821a24f0a62c8db7b234150d655af7c4d087b
chengyunlai <1257997441@qq.com>
2022-12-06 14:35:19 +0800

    了解增删改查

--- src/main/java/spring_08_53_66_jdbc/jdbc_53/anno/JdbcAnnotationApplication.java
+++ src/main/java/spring_08_53_66_jdbc/jdbc_53/anno/JdbcAnnotationApplication.java
@@ -1,8 +1,12 @@
 package spring_08_53_66_jdbc.jdbc_53.anno;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import spring_08_53_66_jdbc.jdbc_53.anno.bean.User;
 import spring_08_53_66_jdbc.jdbc_53.anno.config.JdbcTemplateConfig;
+
+import java.util.List;
 
 /**
  * @ClassName
@@ -15,6 +19,37 @@ public class JdbcAnnotationApplication {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(JdbcTemplateConfig.class);
         JdbcTemplate jdbcTemplate = ctx.getBean(JdbcTemplate.class);
-        jdbcTemplate.execute("insert into tbl_account (user_id, money) values (5, 500)");
+        // jdbcTemplate.execute("insert into tbl_account (user_id, money) values (5, 500)");
+        // 插入
+        int row1 = jdbcTemplate.update("insert into tbl_user (name, tel) values (?, ?)", "heihei", "200");
+        System.out.println(row1);
+
+        // 更新
+        int row2 = jdbcTemplate.update("update tbl_user set tel = ? where name = ?", "54321", "heihei");
+        System.out.println(row2);
+
+        // 删除
+        int row3 = jdbcTemplate.update("delete from tbl_user where name = ?", "heihei");
+        System.out.println(row3);
+
+        // 查询全部
+        List<User> query = jdbcTemplate.query("select * from tbl_user", new BeanPropertyRowMapper<>(User.class));
+        query.forEach(System.out::println);
+
+        System.out.println("===");
+
+        // 条件查询
+        List<User> query2 = jdbcTemplate.query("select * from tbl_user where id > ?", new BeanPropertyRowMapper<>(User.class),2);
+        query2.forEach(System.out::println);
+
+        System.out.println("查询单个");
+        User user = query2.size() > 0 ? query2.get(0) : null;
+        System.out.println(user);
+
+        System.out.println("查询数量");
+        // 查询数量
+        // JdbcTemplate 给我们提供了一些新的方法：queryForObject ，它可以直接指定返回数据要封装的类型。
+        Long count = jdbcTemplate.queryForObject("select count(id) from tbl_user", Long.class);
+        System.out.println(count);
     }
 }
---
+++ src/main/java/spring_08_53_66_jdbc/jdbc_53/anno/bean/User.java
@@ -0,0 +1,23 @@
+package spring_08_53_66_jdbc.jdbc_53.anno.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * @ClassName
+ * @Description
+ * @Author:chengyunlai
+ * @Date
+ * @Version 1.0
+ **/
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private Integer id;
+    private String name;
+    private String tel;
+}
